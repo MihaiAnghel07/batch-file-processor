9973623299b59bb97852823ea17196200bfaafac
MihaiAnghel07 <anghel.imihaigabi@gmail.com>
2024-03-13 21:23:33 +0200

    initial commit

---
+++ src/main/java/EntryPoint.java
@@ -0,0 +1,10 @@
+import org.apache.log4j.Logger;
+
+public class EntryPoint {
+
+    private final static Logger logger = Logger.getLogger(EntryPoint.class);
+
+    public static void main(String[] args) {
+
+    }
+}
